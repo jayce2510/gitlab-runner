bdb4f1d6de8b8f283883968e58c502a15ad95ce9
Jayce Nguyen <alviss2510@gmail.com>
2023-03-29 09:34:44 +0700

    first commit

---
+++ install-gitlab-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#run the compose file
+docker compose up -d
+
+#register the runner inside the container
+docker-compose exec gitlab-runner \                                                                                                          ─╯
+gitlab-runner register \
+--non-interactive \
+--url https://gitlab.cali.vn/ \
+--registration-token GR1348941sXrRxQdqj1i6wHUzX5xD \
+--executor docker \
+--description "docker-runner-199" \
+--docker-image "gitlab/gitlab-runner:alpine" \
+--docker-volumes /var/run/docker.sock:/var/run/docker.sock
